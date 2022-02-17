bac0f72651a935aec33bc86109a315e09855094f
soumyadeep-git <dhalisoumyadeep@gmail.com>
2022-02-17 12:09:52 +0530

    RETURN

---
+++ NewPrograms/Functions/Revision.java
@@ -0,0 +1,9 @@
+public class Revision {
+    public static void printMood() {
+        System.out.println("BRAND NEW");
+    }
+
+    public static void main(String[] args) {
+        printMood();
+    }
+}
